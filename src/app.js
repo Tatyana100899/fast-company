37cacf4e2e905a1f08e3565ed7da92d8cafa93c1
Tatyana <pereverzeva68@gmail.com>
2022-10-30 22:13:05 +0300

    bookmark full

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import API from './api';
 
 function App() {
     const [users, setUsers] = useState(API.users.fetchAll());
-    // console.log(users);
+    
     const handleDelete = (userId) => {       
        setUsers(users.filter((user) => user._id !== userId));             
     };
@@ -13,9 +13,7 @@ function App() {
         
         setUsers((prevState) => {            
             return prevState.map((user) => {
-              if (user._id === userId) {
-              return {...user, bookmark: user.bookmark = !user.bookmark};
-            }              
+              if (user._id === userId) {user.bookmark = !user.bookmark};                        
                 return user
             })
           })
